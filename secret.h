68497afe86710d26ec762637254a84633d6abe65
Aditya BN <aditbn11@gmail.com>
2023-07-25 02:46:59 +0700

    update wifi secret example n httpsAuth

--- secret.h
+++ secret.h
@@ -1,8 +1,12 @@
-#define ssid_AP_1
-#define password_AP_1
+#define ssid_AP_1 "ssid_AP_1"
+#define password_AP_1 "password_AP_1"
 
-#define ssid_AP_2
-#define password_AP_2
+#define ssid_AP_2 "ssid_AP_2"
+#define password_AP_2 "password_AP_2"
 
-#define ssid_AP_3
-#define password_AP_3
+#define ssid_AP_3 "ssid_AP_3"
+#define password_AP_3 "password_AP_3"
+
+#define httpsUname "uname"
+#define httpsPassword "password"
+// usage => https://uname:password@site.example.com/url 
